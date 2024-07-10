1b703afb03a684356ee0a77752ac63bd0de6ed23
Sndinda <102276971+Sndinda@users.noreply.github.com>
2024-07-10 22:22:32 +0530

    Add files via upload

---
+++ flappy bird/player.js
@@ -0,0 +1,10 @@
+class Player{
+    constructor(game){
+        this.game=game;
+        this.x=0;
+        this.y=0;
+        this.width=this.canvas.width;
+        this.height= this.canvas.height;
+    }
+}
+ 
